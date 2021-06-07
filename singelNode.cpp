fe142f1b941c4b23272b737b1e6ef75a5fc12bd9
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2021-06-07 23:05:33 +0200

    Update singelNode.cpp

--- singelNode.cpp
+++ singelNode.cpp
@@ -1,14 +1,14 @@
 #include "singelNode.h"
 #include <iostream>
 
-singelNode::singelNode(string nodeName, string nodeValue) : Node(nodeName)
+singelNode::singelNode(wstring nodeName, wstring nodeValue) : Node(nodeName)
 {
 	this->nodeValue = nodeValue;
 }
 
-void singelNode::saveNode(int spaces  , ofstream & path)
+void singelNode::saveNode(int spaces  , wofstream & path)
 {
 	for (int i = 0; i < spaces; i++)
 		path << "  ";
-	path<< "<" << this->getNodeName() << ">"<<" "<< nodeValue<<" </" << this->getNodeName() << ">\n";
+	path<< "<" << getNodeName() << ">"<< nodeValue<<"</" << getNodeName() << ">\n";
 }
