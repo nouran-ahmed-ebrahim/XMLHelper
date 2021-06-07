723569df6cec4be7c321b43ff120e200733fa685
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2021-06-07 22:58:20 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,75 +1,17 @@
 #include <iostream>
 #include "XMLHelper.h"
+#include <wchar.h>
 
 
 using namespace std;
 
-void addData();
 
 int main()
 {
- // XMLHelper::saveData("XMLFile.xml",XMLHelper::loadData("XMLFile.xml"));
+
+  XMLHelper::saveData("write.xml",XMLHelper::loadData("read.xml"));
 	
-	cout << XMLHelper::cheackAfterRoot("XMLFile.xml");
+
 	return 0;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-void addData()
-{
-	Node* node = new parentNodeClass("root");
-
-	Node* subNode = new parentNodeClass("Students");
-
-	singelNode* tepS = new singelNode("Nouran", "8/5/2001");
-	((parentNodeClass*)subNode)->addNode(tepS);
-
-	tepS = new singelNode("Sama", "15/4/2001");
-    ((parentNodeClass*)subNode)->addNode(tepS);
-
-	tepS = new singelNode("MennaTullah", "23/7/2001");
-	((parentNodeClass*)subNode)->addNode(tepS);
-
-	tepS = new singelNode("Nada", "10/10/2001");
-	((parentNodeClass*)subNode)->addNode(tepS);
-
-	((parentNodeClass*)node)->addNode(subNode);
-
-	XMLData  file(node);
-
-	XMLHelper::saveData("XMLFile.xml" , file);
-
-	delete node, tepS, subNode;
-}
-
-/*
-<Student>
-	<Nouran>
-	  <name>Nouran</name>
-	</Nouran>
-  </Student>
-  <TA>
-	<Ramy>
-	  <Name>Ramy</Name>
-	</Ramy>
-  </TA>
-  */
