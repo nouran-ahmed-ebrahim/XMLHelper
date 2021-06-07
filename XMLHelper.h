b4fa5dbfbac56c1ec17c8005ad9341bf54b2c056
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2021-06-07 23:03:55 +0200

    Update XMLHelper.h

--- XMLHelper.h
+++ XMLHelper.h
@@ -4,20 +4,22 @@
 
 class XMLHelper
 {
-	static string cutName(string, int, int);
-	static string cutValue(string, int, int);
-	static bool validateXMLFile(string);
-	static bool validateBrackts(string);
+    static wstring cutName(wstring, int, int);
+	static wstring cutValue(wstring, int, int);
+	static bool validateBrackets(string);
 	static bool validateTagClosing(string);
 	static bool validateRoot(string);
-	static bool checkNumOfSpaces(string );
-	static bool cheackPostions(int, int);
+	static bool checkNumOfSpaces(string ); 
+	static wstring getLastLine(string);
+	static bool checkBracketsPostions(int, int);
+	static bool cheackAfterRoot(string path);
 
 public:
+	static bool validateXMLFile(string);
 	XMLHelper();
-    static void saveData(string, XMLData);
+        static void saveData(string, XMLData);
 	static XMLData loadData(string);
-	static string cheackAfterRoot(string path);
+	
 
 };
 
