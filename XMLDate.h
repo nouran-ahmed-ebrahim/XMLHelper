5987b7387fe89b2631799196a7ee5d086a562a6d
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2021-06-09 04:12:52 +0200

    XMLData.h

--- XMLDate.h
+++ XMLDate.h
@@ -7,7 +7,8 @@ class XMLData
 	Node* data;
 
 public:
-
+	bool isEmpty();
+	XMLData(); // to create global var it will be filling in loading and it will be used in saving  
 	XMLData(Node*);
 	void savexXMLData(wofstream&);
 };
