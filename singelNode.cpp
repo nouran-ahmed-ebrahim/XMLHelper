5d8b20a0f28d23abe59ca2892ab0076981a63690
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2021-06-13 17:32:05 +0200

    singelNode.cpp

--- singelNode.cpp
+++ singelNode.cpp
@@ -6,9 +6,13 @@ singelNode::singelNode(wstring nodeName, wstring nodeValue) : Node(nodeName)
 	this->nodeValue = nodeValue;
 }
 
-void singelNode::saveNode(int spaces  , wofstream & path)
+void singelNode::saveNode(int spaces  , wofstream & path )
 {
 	for (int i = 0; i < spaces; i++)
 		path << "  ";
-	path<< "<" << getNodeName() << ">"<< nodeValue<<"</" << getNodeName() << ">\n";
+
+	path << "<" << getNodeName() << ">" << nodeValue << "</" << getNodeName() << ">";
+	
+	if(spaces != 0)
+	    path<<"\n";
 }
